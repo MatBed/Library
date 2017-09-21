87b028ff4b9d74487c5f5f357ae52775818c2d38
gerald512 <mateuszbedla@gmail.com>
2017-09-21 20:28:50 +0200

    add load data in admin application and change css

---
+++ AdminApp/Controllers/AdminBooksController.cs
@@ -0,0 +1,46 @@
+﻿using OperationsOnData.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdminApp.Controllers
+{
+    public class AdminBooksController : Controller
+    {
+        private readonly ILibraryOperations libraryOperations;
+
+        public AdminBooksController(ILibraryOperations libraryOperations)
+        {
+            this.libraryOperations = libraryOperations;
+        }
+
+        public ActionResult Index()
+        {
+            //var books = libraryOperations.GetBooks();
+            return View();
+        }
+
+        public ActionResult GetBooks()
+        {
+            var books = libraryOperations.GetBooks();
+            return Json(new { data = books }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                libraryOperations.RemoveBook(id);
+                libraryOperations.SaveChanges();
+            }
+            catch
+            {
+                return View("Index");
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
