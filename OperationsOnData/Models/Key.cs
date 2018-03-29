b6d45e7f6cfc62855a423606409b57c18d381347
Mateusz Bedła <mateuszbedla@gmail.com>
2018-03-29 14:40:39 +0200

    Add key model and viewmodel for adminapp

---
+++ OperationsOnData/Models/Key.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OperationsOnData.Models
+{
+    public class Key
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string AccessKey { get; set; }
+    }
+}
