e139db9c4064831d8bbd248ff69711e41731592c
Mateusz Bedła <mateuszbedla@gmail.com>
2018-04-03 13:47:43 +0200

    add two files to previous commit

---
+++ OperationsOnData/Infrastructure/Concrete/FormsAuthProvider.cs
@@ -0,0 +1,21 @@
+﻿using OperationsOnData.Infrastructure.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace OperationsOnData.Infrastructure.Concrete
+{
+    public class FormsAuthProvider : IAuthProvider
+    {
+        public bool Authenticate(string number, string accessKey)
+        {
+            bool result = FormsAuthentication.Authenticate(number, accessKey);
+            if (result)
+                FormsAuthentication.SetAuthCookie(number, false);
+
+            return result;
+        }
+    }
+}
